014e2e9ba3a5f2f9f97f95b9ccca8658b610ea09
André pereira lima <110937043+zdfgvx@users.noreply.github.com>
2023-09-01 09:35:30 -0300

    Add files via upload

---
+++ 2e-3trim-nutricao-main/js/principal.js
@@ -0,0 +1,18 @@
+
+//atribui o valor da tag h1 para a variavel titulo
+
+let titulo = document.querySelector("h1").textContent;
+
+//mostra no console do navegador a variavel titulo
+
+console.log(titulo);
+
+//modifica o titulo para "trembolona kkkkkkk"
+
+document.querySelector("h1").textContent = "trembolona kkkkkkk";
+
+//««««««««««««««aqui vamos maxer na tabela de imc»»»»»»»»»»»»»»»»//
+
+let paciente = document.querySelector("#primeiro-paciente");
+
+console.log(paciente)
